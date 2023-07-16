4b9c8a512d104386f3258d876e9c20047d8c1426
chamNN <champhan625@gmail.com>
2023-07-16 09:01:08 +0700

    init project

---
+++ frm_onTaplab3.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace onTapLab3
+{
+    partial class frm_onTaplab3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstB_chonLop = new System.Windows.Forms.ComboBox();
+            this.lstv_danhSachLop = new System.Windows.Forms.ListView();
+            this.lbl_chonLop = new System.Windows.Forms.Label();
+            this.btn_xuatThongTin = new System.Windows.Forms.Button();
+            this.lbl_ds = new System.Windows.Forms.Label();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstB_chonLop
+            // 
+            this.lstB_chonLop.FormattingEnabled = true;
+            this.lstB_chonLop.Location = new System.Drawing.Point(303, 86);
+            this.lstB_chonLop.Name = "lstB_chonLop";
+            this.lstB_chonLop.Size = new System.Drawing.Size(181, 24);
+            this.lstB_chonLop.TabIndex = 0;
+            this.lstB_chonLop.SelectedIndexChanged += new System.EventHandler(this.lstB_chonLop_SelectedIndexChanged);
+            // 
+            // lstv_danhSachLop
+            // 
+            this.lstv_danhSachLop.GridLines = true;
+            this.lstv_danhSachLop.HideSelection = false;
+            this.lstv_danhSachLop.Location = new System.Drawing.Point(51, 167);
+            this.lstv_danhSachLop.Name = "lstv_danhSachLop";
+            this.lstv_danhSachLop.Size = new System.Drawing.Size(701, 256);
+            this.lstv_danhSachLop.TabIndex = 1;
+            this.lstv_danhSachLop.UseCompatibleStateImageBehavior = false;
+            this.lstv_danhSachLop.View = System.Windows.Forms.View.Details;
+            this.lstv_danhSachLop.SelectedIndexChanged += new System.EventHandler(this.lstv_danhSachLop_SelectedIndexChanged);
+            // 
+            // lbl_chonLop
+            // 
+            this.lbl_chonLop.AutoSize = true;
+            this.lbl_chonLop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_chonLop.Location = new System.Drawing.Point(198, 90);
+            this.lbl_chonLop.Name = "lbl_chonLop";
+            this.lbl_chonLop.Size = new System.Drawing.Size(80, 20);
+            this.lbl_chonLop.TabIndex = 2;
+            this.lbl_chonLop.Text = "Chọn lớp:";
+            // 
+            // btn_xuatThongTin
+            // 
+            this.btn_xuatThongTin.Location = new System.Drawing.Point(320, 454);
+            this.btn_xuatThongTin.Name = "btn_xuatThongTin";
+            this.btn_xuatThongTin.Size = new System.Drawing.Size(122, 35);
+            this.btn_xuatThongTin.TabIndex = 3;
+            this.btn_xuatThongTin.Text = "xuất thông tin";
+            this.btn_xuatThongTin.UseVisualStyleBackColor = true;
+            this.btn_xuatThongTin.Click += new System.EventHandler(this.btn_xuatThongTin_Click);
+            // 
+            // lbl_ds
+            // 
+            this.lbl_ds.AutoSize = true;
+            this.lbl_ds.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ds.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.lbl_ds.Location = new System.Drawing.Point(196, 24);
+            this.lbl_ds.Name = "lbl_ds";
+            this.lbl_ds.Size = new System.Drawing.Size(342, 32);
+            this.lbl_ds.TabIndex = 4;
+            this.lbl_ds.Text = "DANH SÁCH SINH VIÊN";
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.Location = new System.Drawing.Point(524, 454);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(122, 35);
+            this.btn_xoa.TabIndex = 5;
+            this.btn_xoa.Text = "xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            this.btn_xoa.Click += new System.EventHandler(this.btn_xoa_Click);
+            // 
+            // frm_onTaplab3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 510);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.lbl_ds);
+            this.Controls.Add(this.btn_xuatThongTin);
+            this.Controls.Add(this.lbl_chonLop);
+            this.Controls.Add(this.lstv_danhSachLop);
+            this.Controls.Add(this.lstB_chonLop);
+            this.Name = "frm_onTaplab3";
+            this.Text = "bài làm";
+            this.Load += new System.EventHandler(this.frm_onTaplab3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox lstB_chonLop;
+        private System.Windows.Forms.ListView lstv_danhSachLop;
+        private System.Windows.Forms.Label lbl_chonLop;
+        private System.Windows.Forms.Button btn_xuatThongTin;
+        private System.Windows.Forms.Label lbl_ds;
+        private System.Windows.Forms.Button btn_xoa;
+    }
+}
+
---
+++ frm_onTaplab3.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace onTapLab3
+{
+    public partial class frm_onTaplab3 : Form
+    {
+        public frm_onTaplab3()
+        {
+            InitializeComponent();
+        }
+
+        string ketNoi = "Data Source=LAPTOP-JAKJA372\\TAIMAIKHONGDUOC;Initial Catalog=onTapLab_LT3;Integrated Security=True";
+        SqlConnection conn = null;
+
+        private void frm_onTaplab3_Load(object sender, EventArgs e)
+        {
+            ColumnHeader masv = new ColumnHeader();
+            masv.Text = "Mã Sinh Viên";
+            masv.Width = 100;
+
+            ColumnHeader tensv = new ColumnHeader();
+            tensv.Text = "Họ và tên Tên";
+            masv.Width = 150;
+            
+            ColumnHeader gioiTinh = new ColumnHeader();
+            gioiTinh.Text = "Giới Tính";
+            gioiTinh.Width = 80; 
+            
+            ColumnHeader diaChi = new ColumnHeader();
+            diaChi.Text = "Địa Chỉ";
+            diaChi.Width = 200;
+
+            lstv_danhSachLop.Columns.Add(masv);
+            lstv_danhSachLop.Columns.Add(tensv);
+            lstv_danhSachLop.Columns.Add(gioiTinh);
+            lstv_danhSachLop.Columns.Add(diaChi);
+
+            try
+            {
+                conn = new SqlConnection(ketNoi);
+                conn.Open();
+
+                string xuatLop = @"SELECT * FROM lop";
+
+                SqlCommand cmd = new SqlCommand(xuatLop, conn);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    string TenLop = rdr["tenLop"].ToString();
+                    lstB_chonLop.Items.Add(TenLop);
+                }
+
+                rdr.Close();
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi " + ex, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+
+
+        }
+
+        private void lstB_chonLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void lstv_danhSachLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_xuatThongTin_Click(object sender, EventArgs e)
+        {
+            lstv_danhSachLop.Items.Clear();
+            if(lstB_chonLop.SelectedItem == null)
+            {
+                MessageBox.Show("Vui lòng chọn một lớp.", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            string CMB_tenLop = lstB_chonLop.SelectedItem.ToString();
+            try
+            {
+
+
+                using (conn = new SqlConnection(ketNoi))
+                {
+                    conn.Open();
+
+
+                    /*string xuatSinhVien = @"SELECT sinhVien.maSV, sinhVien.hoTen, sinhVien.gioiTinh, sinhVien.diaChi FROM sinhVien
+                                      INNER JOIN lop ON sinhVien.MaLop = lop.MaLop
+                                      WHERE lop.tenLop = @TenLop";*/
+
+                    string xuatRa = @"SELECT sinhVien.maSV, sinhVien.hoTen, sinhVien.gioiTinh, sinhVien.diaChi FROM sinhVien, lop WHERE lop.tenLop = @TenLop AND sinhVien.maLop = lop.maLop";
+                    SqlCommand cmd = new SqlCommand(xuatRa, conn);
+                    cmd.Parameters.AddWithValue("@TenLop", CMB_tenLop);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        string masv = rdr["maSV"].ToString();
+                        string hoten = rdr["hoTen"].ToString();
+                        string GioiTinh = rdr["gioiTinh"].ToString();
+                        string DiaChi = rdr["diaChi"].ToString();
+
+                        ListViewItem item = new ListViewItem();
+                        item.Text = masv;
+                        item.SubItems.Add(hoten);
+                        item.SubItems.Add(GioiTinh);
+                        item.SubItems.Add(DiaChi);
+                        lstv_danhSachLop.Items.Add(item);
+                        lstv_danhSachLop.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                        lstv_danhSachLop.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                    }
+                    rdr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem người dùng đã chọn một sinh viên trong ListView chưa
+            if (lstv_danhSachLop.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để xóa.", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Xác nhận xóa sinh viên
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa sinh viên này?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+                return;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ketNoi))
+                {
+                    connection.Open();
+
+                    // Lấy mã sinh viên của sinh viên được chọn trong ListView
+                    string maSV = lstv_danhSachLop.SelectedItems[0].Text;
+
+                    // Xóa sinh viên khỏi CSDL
+                    string sql = "DELETE FROM SinhVien WHERE maSV = @MaSV";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@MaSV", maSV);
+                    command.ExecuteNonQuery();
+
+                    // Xóa sinh viên khỏi ListView
+                    lstv_danhSachLop.SelectedItems[0].Remove();
+
+                    MessageBox.Show("Đã xóa sinh viên thành công.", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
